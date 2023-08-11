10e3cb610d53ebf6bc3b7bda04a51cd9de3fbdf5
Siklink <oladunjoyeolaoluwa.j@gmail.com>
2023-08-11 06:35:07 -0700

    My first C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C program that print a line with puts function
+ * Return: 0 (Successful)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
